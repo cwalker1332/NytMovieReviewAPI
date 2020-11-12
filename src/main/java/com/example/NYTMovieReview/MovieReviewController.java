5bd48f83e0bfd7266dcf9b9ba8a59f1b040ad125
Student <cwalker1332@gmail.com>
2020-11-12 15:22:08 -0600

    fixing typo

--- src/main/java/com/example/NYTMovieReview/MovieReviewController.java
+++ src/main/java/com/example/NYTMovieReview/MovieReviewController.java
@@ -13,7 +13,7 @@ public class MovieReviewController {
     // create result/movie review 1
     private Link link1 = new Link("article", "http://www.nytimes.com/2019/06/06/movies/the-black-godfather-review.html", "Read the New York Times Review of The Black Godfather");
     private Multimedia multimedia1 = new Multimedia("mediumThreeByTwo210", "https://static01.nyt.com/images/2019/06/05/arts/blackgodfather1/blackgodfather1-mediumThreeByTwo210.jpg", 210, 140);
-    private Results result1 = new Results("The Black Godfather", "", 0, "BEN KENIGSBERG", "'The Black Godfather'", "Reginald Hudlin’s documentary about Clarence Avant includes many golden anecdotes.", "2019-06-06", "2019-06-07", "2019-06-14 16:44:01", link1, multimedia1);
+    private Results result1 = new Results("The Black Godfather", "", 0, "BEN KENIGSBERG", "‘The Black Godfather’ Review: The Music Executive Who Made It All Happen", "Reginald Hudlin’s documentary about Clarence Avant includes many golden anecdotes.", "2019-06-06", "2019-06-07", "2019-06-14 16:44:01", link1, multimedia1);
 
     // create result/movie review 2
     private Link link2 = new Link("article", "http://www.nytimes.com/2011/04/15/movies/square-grouper-movie-review.html","Read the New York Times Review of Square Grouper: The Godfathers of Ganja");
